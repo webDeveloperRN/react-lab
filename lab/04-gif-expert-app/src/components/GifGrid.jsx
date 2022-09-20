160aa28e530c58ba60134eb592df8e7274d8933e
pikino84 <pikino84@gmail.com>
2022-09-20 17:05:25 -0500

    update

--- lab/04-gif-expert-app/src/components/GifGrid.jsx
+++ lab/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
+
 export const GifGrid = ( { category } ) => {
-    getGifs(category);
+
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs( category );
+        setImages( newImages );
+    }
+
+    useEffect( () => {
+        getImages();
+    }, []);
+    
+    
     return(
         <>
             <h3>{ category }</h3>
+            <ol>
+                <li>Title</li>
+            </ol>
         </>
     )
 }
--- lab/04-gif-expert-app/src/helpers/getGifs.js
+++ lab/04-gif-expert-app/src/helpers/getGifs.js
@@ -4,13 +4,12 @@ export const getGifs = async( category ) => {
     const resp = await fetch( url );
     const { data } = await resp.json();
     
-    const gifs = data.map ( img => ({
+    const gifs = data.map( img => ({
         id: img.id,
         title: img.title,
         url: img.images.downsized_medium.url
     }));
 
-    console.log(gifs);
     return gifs;
     
 }
